76b33f230225256341fb4451d3b0ec566c48a9e0
Viacheslav Kostenko <shegan782@gmail.com>
2021-03-30 17:36:35 +0300

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="logo" />
+        </header>
+    );
+};
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className='img-main'>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="main pic" />
+            </div>
+            <div>
+                ava + descr
+            </div>
+            <div>
+                my posts
+            <div>
+                new post
+            </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Profile;
